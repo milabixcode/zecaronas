70080927906e36a4bda3acafceb361e1a8626084
milabixcode <camila_ferrari@id.uff.br>
2024-04-14 21:52:52 -0300

    first commit

---
+++ src/main/java/gpms/zecaronas/ZecaronasApplication.java
@@ -0,0 +1,13 @@
+package gpms.zecaronas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZecaronasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZecaronasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gpms/zecaronas/ZecaronasApplicationTests.java
@@ -0,0 +1,13 @@
+package gpms.zecaronas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZecaronasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
